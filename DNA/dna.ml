19130e0bfc5cf98580ca3f68594c1863746a4cd0
Zoe P <zoe.paraskevopoulou@gmail.com>
2012-11-03 20:59:27 +0200

    fun with dna

--- DNA/dna.ml
+++ DNA/dna.ml
@@ -4,7 +4,8 @@ let solve =
   let t = ref 0 in
   let g = ref 0 in
   let c = ref 0 in
-    String.iter (function | 'A' -> a := !a + 1
+    String.iter (function 
+                   | 'A' -> a := !a + 1
                    | 'T' -> t := !t + 1
                    | 'G' -> g := !g + 1
                    | 'C' -> c := !c + 1
